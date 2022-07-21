95dbcc79b1487f2c041375311d1fd81a8dcf55c7
Siddhant <santoshshrivastava39@gmail.com>
2022-07-21 14:08:07 +0530

    Create Address Book table in Database

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -3,3 +3,17 @@
 create database AddressBookService
 
 use AddressBookService
+
+--UC2 Create address book table
+create table Address_Book(
+FirstName varchar(20),
+LastName varchar(20),
+Address varchar(50),
+City varchar(20),
+State varchar(20),
+ZipCode int,
+PhoneNumber bigint,
+Email varchar(25)
+)
+--To get data from table
+select * from Address_Book
