4df89d34765615b2a989da66ad8700565f9136fb
Siddhant <santoshshrivastava39@gmail.com>
2022-07-21 14:19:58 +0530

    Insert new contacts into address book

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -17,3 +17,10 @@ Email varchar(25)
 )
 --To get data from table
 select * from Address_Book
+
+--UC3 Insert contacts into address book
+insert into Address_Book values 
+('Siddhant','Shrivastava','A colony','Ujjain','MP',666666,9998889998,'Sid@gmail.com'),
+('Riya','Sharma','Ab colony','Lucknow','UP',677666,9998882348,'Riya@gmail.com'),
+('Pranav','Vyas','C colony','Pune','Maharashtra',656666,9923489998,'Pranav@gmail.com'),
+('Anamika','Singh','D colony','Chennai','Tamil Naidu',662226,8898889998,'Anamika@gmail.com')
